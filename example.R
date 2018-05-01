93d53f1de79bf903973922c1a654e83aed331829
Chuliang Song <33292882+clsong@users.noreply.github.com>
2018-05-01 10:00:18 -0400

    Update example.R

--- example.R
+++ example.R
@@ -27,9 +27,9 @@ Omega_overlap(A, B) #overlap of two interaction matrices
 
 set.seed(4)
 A <- interaction_matrix_random(3, 0.4, 1) #generate a random interaction matrix
-C <- diag(c(-1,-1,-1), 3) #imposing biological constraint. Here it refers to that the growth rates of all species have to be positive
+C1 <- diag(c(-1,-1,-1), 3) #imposing biological constraint. Here it refers to that the growth rates of all species have to be positive
+C2 <- diag(c(1,-1,1), 3)  #imposing biological constraint. Here it refers to that the growth rates of species 1 and 3 have to be positive, and the growth rates of species 2 has to be negative
 
 Omega(A) #relative size of the original interaction matrix
-Omega_overlap(A, C) #the normalized size of the feasibility domain of a random interaction matrix under linear biological constriants
-
-
+Omega_overlap(A, C1) #the normalized size of the feasibility domain of a random interaction matrix under linear biological constriants C1
+Omega_overlap(A, C2) #the normalized size of the feasibility domain of a random interaction matrix under linear biological constriants C2
