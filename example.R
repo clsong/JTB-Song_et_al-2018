860007e4fa09f15697e2fd7acd07017294800c73
Chuliang Song <33292882+clsong@users.noreply.github.com>
2018-05-01 10:32:33 -0400

    Update example.R

--- example.R
+++ example.R
@@ -23,12 +23,12 @@ Omega(A) #relative size of interaction matrix
 Omega(B) #relative size of interaction matrix
 Omega_overlap(A, B) #overlap of two interaction matrices
 
-# example of the normalized size of the feasibility domain of a random interaction matrix under linear biological constriants-----------------
+# example of the normalized size of the feasibility domain of a random interaction matrix under one linear biological constriants-----------------
 
 set.seed(4)
 A <- interaction_matrix_random(3, 0.4, 1) #generate a random interaction matrix
 C1 <- diag(c(-1,-1,-1), 3) #imposing biological constraint. Here it refers to that the growth rates of all species have to be positive
-C2 <- diag(c(1,-1,1), 3)  #imposing biological constraint. Here it refers to that the growth rates of species 1 and 3 have to be positive, and the growth rates of species 2 has to be negative
+C2 <- diag(c(1,-1,1), 3)  #imposing biological constraint. Here it refers to that the growth rates of species 1 and 3 have to be negative, and the growth rates of species 2 has to be positive
 
 Omega(A) #relative size of the original interaction matrix
 Omega_overlap(A, C1) #the normalized size of the feasibility domain of a random interaction matrix under linear biological constriants C1
